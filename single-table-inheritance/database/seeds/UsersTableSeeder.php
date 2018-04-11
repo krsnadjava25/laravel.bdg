ae075e0f601a423d63beb3d043f5eda781b2a5dd
Krisna Fathurahman <krisna.fathurahman@divusi.com>
2018-04-11 17:11:26 +0700

    Factory and Seeder

---
+++ single-table-inheritance/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->states('tester')->create();
+        factory(App\User::class, 10)->create();
+    }
+}
