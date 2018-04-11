ed3e5de2d4961220c7dbbfed50205acff80cb91c
Krisna Fathurahman <krisna.fathurahman@divusi.com>
2018-04-11 16:57:15 +0700

    Basic Eloquent ORM & Migrations

---
+++ single-table-inheritance/database/migrations/2018_04_11_095336_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('type', ['feature', 'bug'])->default('feature');
+            $table->integer('project_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('projects')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('reporter_id')->unsigned()->nullable();
+            $table->foreign('reporter_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
